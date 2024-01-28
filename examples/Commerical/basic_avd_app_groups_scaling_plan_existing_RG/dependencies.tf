440e2b1f455e74c8eb30f5d333ff3f6a3c41fc37
John Spinella <jrspinella@users.noreply.github.com>
2024-01-28 11:50:41 -0500

    Add examples

---
+++ examples/Commerical/basic_avd_app_groups_scaling_plan_existing_RG/dependencies.tf
@@ -0,0 +1,76 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+/* data "azuread_group" "vm_admins_group" {
+  display_name = "Virtual Machines Admins"
+}
+
+data "azuread_group" "vm_users_group" {
+  display_name = "Virtual Machines Users"
+}
+ */
+ resource "azurerm_resource_group" "avd-network-rg" {
+  name     = "avd-network-rg"
+  location = var.location
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_resource_group" "avd-rg" {
+  name     = "avd-rg"
+  location = var.location
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_virtual_network" "avd-vnet" {
+  depends_on = [
+    azurerm_resource_group.avd-network-rg
+  ]
+  name                = "avd-network"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.avd-network-rg.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_subnet" "avd-snet" {
+  depends_on = [
+    azurerm_resource_group.avd-network-rg,
+    azurerm_virtual_network.avd-vnet
+  ]
+  name                 = "avd-subnet"
+  resource_group_name  = azurerm_resource_group.avd-network-rg.name
+  virtual_network_name = azurerm_virtual_network.avd-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "avd-nsg" {
+  depends_on = [
+    azurerm_resource_group.avd-network-rg,
+  ]
+  name                = "avd-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.avd-network-rg.name
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "avd-log" {
+  depends_on = [
+    azurerm_resource_group.avd-network-rg
+  ]
+  name                = "avd-log"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.avd-network-rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags = {
+    environment = "test"
+  }
+}
---
+++ examples/Commerical/basic_avd_mult_apps_new_rg/main.tf
@@ -0,0 +1,145 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Terraform module for deploying a basic Azure Desktop with Application Group in Azure. 
+
+module "mod_avd" {
+  #source  = "azurenoops/overlays-desktop-virtualization/azurerm"
+  #version = "x.x.x"
+  source = "../../.."
+
+  depends_on = [
+    azurerm_log_analytics_workspace.avd-log,
+  ]
+
+  # Resource Group, location, VNet and Subnet details
+  create_avd_resource_group = true
+  location                  = var.location
+  deploy_environment        = var.deploy_environment
+  environment               = var.environment
+  org_name                  = var.org_name
+  workload_name             = var.workload_name
+
+  # AVD Workspace details
+  avd_workspace_config = {
+    public_network_access_enabled = false
+    add_tags = {
+      foo = "bar"
+    }
+  }
+
+  # AVD Host Pool details
+  avd_host_pool_config = {
+    # Value will automatically change depending on the Scaling Plan settings
+    load_balancer_type = "BreadthFirst"
+
+    scheduled_agent_updates = {
+      enabled = true
+      schedules = [
+        {
+          day_of_week = "Sunday"
+          hour_of_day = 8
+        },
+        {
+          day_of_week = "Wednesday"
+          hour_of_day = 22
+        },
+      ]
+    }
+  }
+
+  # AVD Application Group details
+  avd_application_group_config = {
+    type = "Application"
+  }
+
+  # AVD Application details
+  avd_application_config = {
+    notepad = {
+      app_name                     = "Notepad"
+      friendly_name                = "Notepad"
+      description                  = "Description"
+      path                         = "C:\\Windows\\System32\\notepad.exe"
+      command_line_argument_policy = "Allow"
+      aad_group                    = "SG-BlueApp-Users"
+      add_tags                     = {}
+    },
+    calc = {
+      app_name                     = "Calculator"
+      friendly_name                = "Calculator"
+      description                  = "Description"
+      path                         = "C:\\Windows\\System32\\calc.exe"
+      command_line_argument_policy = "Allow"
+      aad_group                    = "SG-RedApp-Users"
+      add_tags                     = {}
+    }
+  }
+
+  avd_vm_config = {
+    windows2019 = {
+      os_type                                      = "windows"
+      distribution_name                            = "windows2019dc"
+      virtual_machine_size                         = "Standard_D13_v2"
+      disable_password_authentication              = true
+      aad_group_desktop                            = "SG-AVD-PersonalDesktop-Users"
+      admin_username                               = "azureuser"
+      admin_password                               = "P@ssw0rd1234"
+      instances_count                              = 5
+      private_ip_address_allocation_type           = "Dynamic"
+      existing_virtual_network_resource_group_name = azurerm_resource_group.avd-network-rg.name
+      existing_virtual_network_name                = azurerm_virtual_network.avd-vnet.name
+      existing_subnet_name                         = azurerm_subnet.avd-snet.name
+      existing_network_security_group_name         = azurerm_network_security_group.avd-nsg.name
+      nsg_inbound_rules = [
+        {
+          name                       = "RDP"
+          priority                   = 100
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "3389"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        {
+          name                       = "HTTP"
+          priority                   = 101
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "80"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        {
+          name                       = "HTTPS"
+          priority                   = 102
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+      ]
+      data_disks = [
+        {
+          name                 = "disk1"
+          disk_size_gb         = 100
+          storage_account_type = "StandardSSD_LRS"
+        }
+      ]
+      add_tags = {
+        foo = "windows2019"
+      }
+    }
+  }
+
+  # Adding additional TAG's to your Azure resources
+  add_tags = {
+    Example = "basic_avd_mult_app_groups_using_existing_RG"
+  }
+}
